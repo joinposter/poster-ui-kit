13661cdf9399e19b97191a57a8fe0973d3d212b8
Vladimir Ivanchenko <vlm.ivanchenko@gmail.com>
2019-01-23 21:23:22 +0200

    Final first release

--- webpack.config.js
+++ webpack.config.js
@@ -6,84 +6,89 @@ var build = process.env.NODE_ENV === 'production';
 
 
 process.argv.forEach((arg) => {
-	arg = arg.split('.')[1];
+    arg = arg.split('.')[1];
 
-	if (arg === 'p' || arg === 'production') {
-		build = true;
-	}
+    if (arg === 'p' || arg === 'production') {
+        build = true;
+    }
 });
 
 module.exports = {
-	context: __dirname,
-	entry: {
-		'bundle.js': './src/index.js',
-	},
+    context: __dirname,
+    entry: {
+        'bundle.js': './src/index.js',
+        'bundle.dist.js': './src/export.js',
+    },
 
-	resolve: {
-		extensions: ['.json', '.js', '.jsx', '.less'],
-	},
+    resolve: {
+        extensions: ['.json', '.js', '.jsx', '.less'],
+        alias: {
+            'poster-ui-kit': path.resolve(__dirname, 'src/export.js'),
+        },
+    },
 
-	output: {
-		path: path.resolve(__dirname, './'),
-		filename: '[name]',
-	},
+    output: {
+        path: path.resolve(__dirname, './'),
+        filename: '[name]',
+        libraryTarget: 'commonjs2'
+    },
 
-	module: {
-		rules: [
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader?cacheDirectory',
-				query: {
-					presets: ['react', 'es2015', 'stage-0'],
-				},
-			},
-			{
-				test: /\.css$/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-				],
-			},
-			{
-				test: /\.less/,
-				use: [
-					{ loader: 'style-loader' },
-					{ loader: 'css-loader' },
-					{ loader: 'less-loader' },
-				],
-			},
-			{
-				test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-				loader: 'url-loader',
-			},
-		],
-	},
-	devtool: build ? 'source-map' : 'eval',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader?cacheDirectory',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                ],
+            },
+            {
+                test: /\.less/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' },
+                ],
+            },
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+                loader: 'url-loader',
+            },
+        ],
+    },
+    devtool: build ? 'source-map' : 'eval',
 
-	watch: !build,
+    watch: !build,
 
-	watchOptions: {
-		aggregateTimeout: 100,
-	},
+    watchOptions: {
+        aggregateTimeout: 100,
+    },
 
-	plugins: [],
+    plugins: [],
 };
 
 if (build) {
-	module.exports.plugins.push(
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify('production'),
-			},
-		}),
-	);
-	module.exports.plugins.push(
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				unsafe: true,
-				warnings: false,
-			},
-			sourceMap: true,
-		}),
-	);
+    module.exports.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+    );
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                unsafe: true,
+                warnings: false,
+            },
+            sourceMap: true,
+        }),
+    );
 }
