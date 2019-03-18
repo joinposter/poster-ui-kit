a2939a5efef7cf20b1b7d1ca6707dabc7004a2f7
Evgeniy Polovniy <evgeniypolovniy@gmail.com>
2019-03-18 09:48:12 +0200

    Add export one component

--- src/build.js
+++ src/build.js
@@ -3,7 +3,9 @@ import './css/main.less';
 import Button from './js/Button';
 import Spinner from './js/Spinner';
 
-
+// For exporting All components
 const PosterUiKit = { Button, Spinner };
 
+// For export only one component
+export { Button, Spinner };
 export default PosterUiKit;
